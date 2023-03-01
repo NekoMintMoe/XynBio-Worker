76d8c4e62ac16fe009773dba2db978cd69a696e0
Season Shi <seasonshi@elyart.dev>
2023-03-02 00:28:50 +0800

    Read posts dir but buggy

--- app/api/blog.ts
+++ app/api/blog.ts
@@ -1,22 +1,32 @@
 import { commonResponse, jsonResponse, jwtDetect } from '../../lib/utils'
 import { Handler } from 'worktop'
+import * as fs from 'fs'
+import * as matter from 'gray-matter';
 
 export const BlogListHandler: Handler = async function (req, res) {
-    if (!await jwtDetect(req)) return await commonResponse(res, 401)
-    
-    const metadata = {
-      title: 'Hello World',
-      subtitle: 'This is a subtitle',
-      date: '2021-01-01',
-      author: 'John Doe',
-      keyword: 'hello world',
-      slug: 'hello-world',
-    }
-    const link = 'https://github.com'
-    const DataArray = [{ link: link, metadata: metadata }, { link: link, metadata: metadata }]
+    //if (!await jwtDetect(req)) return await commonResponse(res, 401)
+
+    const files = fs.readdirSync(SUB_DIR_NAME + '/posts/blog')
+    const blogs = files.map((fileName) => {
+        const slug = fileName.replace('.md', '')
+        const readFile = fs.readFileSync(SUB_DIR_NAME + '/posts/blog/' + fileName, 'utf8')
+        const origmeta = matter(readFile).data
+        const link = NEXT_WEB_URL + '/blog/' + slug
+        const metadata = {
+            title: origmeta.title,
+            subtitle: origmeta.subtitle,
+            date: origmeta.date,
+            author: origmeta.author,
+            keyword: origmeta.keyword,
+            slug: slug,
+        }
+        const DataArray = { link: link, metadata: metadata }
+        return DataArray
+    })
+    console.log(blogs)
     const data = {
       code: 200,
-      data: DataArray
+      data: "1"
     }
   
     return await jsonResponse(res, 200, data)
